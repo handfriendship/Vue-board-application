52f432c60b0cbc7a9d8de3db6fac1f21ac87d90a
sonwj77@naver.com <xenmedia@SONWOOJUNGs-MacBook-Pro.local>
2020-04-03 22:27:50 -0700

    backup

--- src/App.vue
+++ src/App.vue
@@ -20,6 +20,16 @@
     <button type="button" @click="clearLocalstorage">clear localStorage</button>
     <br/>this.users : {{users}}
     <br/> this.currentUser : {{currentUser}}
+    <div class="dropdown">
+      <button type="button" class="btn btn-primary dropdown-toggle" data-toggle="dropdown">
+        Dropdown button
+      </button>
+      <div class="dropdown-menu">
+        <a class="dropdown-item" href="#">Link 1</a>
+        <a class="dropdown-item" href="#">Link 2</a>
+        <a class="dropdown-item" href="#">Link 3</a>
+      </div>
+    </div>
   </div>
 </template>
 
